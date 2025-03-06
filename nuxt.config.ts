0a0532a740c8557f0d19a03655da8e9809eeec28
vitali <v.jalbu31@gmail.com>
2025-03-06 10:41:44 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { themeSettings } from "./theme";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
-  ssr: false,
+  ssr: true,
   devtools: { enabled: true },
   modules: ["@nuxt/ui"],
   css: ["~/assets/css/main.css"],
