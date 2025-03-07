73fb20e8e6063cc50288436c5dd4d5743f690174
pawas007 <trushostap@gmail.com>
2025-03-07 19:51:53 +0200

    Init

---
+++ components/TravelCardNew.vue
@@ -0,0 +1,13 @@
+<template>
+    <UCard class="transform transition hover:-translate-y-1 duration-300">
+
+      asda
+            </UCard>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+
+</style>
